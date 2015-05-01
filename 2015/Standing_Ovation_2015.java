52dc84906d826784885424a7e678cb11095a2f1e
Andres Ortiz <andresortiz28@hotmail.com>
2015-04-30 23:47:33 -0500

    Standing Ovation

---
+++ 2015/Standing_Ovation_2015.java
@@ -0,0 +1,54 @@
+package googlecodejam;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Standing_Ovation {
+
+	public static void main(String[] args) throws Exception
+	{
+		BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream("D:/A-large.in")));
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("D:/output.txt")));
+		String line;		
+		StringTokenizer tokens;
+		line = in.readLine();
+		int n = Integer.parseInt(line);			
+		for (int i = 0; i < n; i++) 
+		{
+			line = in.readLine();
+			tokens = new StringTokenizer(line);
+			
+			tokens.nextToken();
+			String ovation = tokens.nextToken();				
+			
+			int sum = 0;
+			int need = 0;
+			for (int j = 0; j < ovation.length(); j++) 
+			{
+				int a  = Integer.parseInt(ovation.charAt(j)+"");
+				if(a > 0)
+				{
+					if(sum >= j)
+						sum += a;
+					else
+					{
+						need += (j - sum);
+						sum = j + a;
+					}
+				}					
+			}
+			out.write("Case #"+(i+1)+": "+need+"\n");			
+		}
+		in.close();
+		out.flush();
+		out.close();
+		System.exit(0);
+
+	}
+
+}
